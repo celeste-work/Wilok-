5e7d03c3533961f7ede8ef96a6c29146976e81ab
Celeste Aquino <celesteaquino12@gmail.com>
2025-03-07 01:45:09 -0300

    Delete unnecessary files

--- sign-up/script.js
+++
@@ -1,8 +0,0 @@
-const botonIngresar = document.getElementById('botonIngresar');
-
-botonIngresar.onclick = function() {
-    alert("Iniciando sesión...");
-    // Aquí puedes agregar la lógica para la acción de iniciar sesión
-    // Por ejemplo, redirigir a otra página:
-    window.location.href = "../LOGIN/index.html";
-};
